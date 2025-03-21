f47beb6db741aa4e9d01e8e4329701a860184791
SachindaBandara <sachindaudeni192@gmail.com>
2025-03-21 14:44:34 +0530

    create database connection

---
+++ phpbasic/mysql/databaseConnection.php
@@ -0,0 +1,18 @@
+<?php
+$db_server = "127.0.0.1:3308";
+$db_user = "root";
+$db_password = "";
+$db_name = "sachinda";
+$conn = "";
+
+try{
+$conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+if($conn){
+    echo "connected successfully!";
+}
+
+}catch(mysqli_sql_exception){
+    echo "Not connected!";
+}
+?>
